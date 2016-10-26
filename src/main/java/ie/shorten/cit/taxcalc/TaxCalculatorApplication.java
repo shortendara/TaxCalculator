7063202318634217e7a9761d4c90920c616fd42c
Dara Shorten <Dara Shorten>
2016-10-26 17:20:24 +0100

    Initial Commit

---
+++ src/main/java/ie/shorten/cit/taxcalc/TaxCalculatorApplication.java
@@ -0,0 +1,12 @@
+package ie.shorten.cit.taxcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxCalculatorApplication.class, args);
+	}
+}
