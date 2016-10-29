a4a0fc3938171a5e9bd764f35c987f4af4165b43
Dara Shorten <Dara Shorten>
2016-10-29 12:47:20 +0100

    Extra functions included to include cur off rates

--- src/main/java/ie/shorten/cit/taxcalc/domain/TaxBandsImpl.java
+++ src/main/java/ie/shorten/cit/taxcalc/domain/TaxBandsImpl.java
@@ -4,10 +4,37 @@ import ie.shorten.cit.taxcalc.interfaces.TaxBands;
 
 public class TaxBandsImpl implements TaxBands {
 
+	private double exempt_limit;
+	private double lower_limit;
 	private double lower_rate;
+	private double middle_limit;
 	private double middle_rate;
 	private double higher_rate;
 	private double solitary_rate;
+	
+	@Override
+	public void set_exempt_limit(double exempt_limit) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public double get_exempt_limit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void set_lower_limit(double lower_limit) {
+		// TODO Auto-generated method stub
+		this.lower_limit = lower_limit;
+	}
+	
+	@Override
+	public double get_lower_limit() {
+		// TODO Auto-generated method stub
+		return lower_limit;
+	}
 	
 	@Override
 	public void set_lower_rate(double lower_rate) {
@@ -21,6 +48,18 @@ public class TaxBandsImpl implements TaxBands {
 		return lower_rate;
 	}
 
+	@Override
+	public void set_middle_limit(double middle_limit) {
+		// TODO Auto-generated method stub
+		this.middle_limit = middle_limit;
+	}
+
+	@Override
+	public double get_middle_limit() {
+		// TODO Auto-generated method stub
+		return middle_limit;
+	}
+	
 	@Override
 	public void set_middle_rate(double middle_rate) {
 		// TODO Auto-generated method stub
@@ -56,5 +95,4 @@ public class TaxBandsImpl implements TaxBands {
 		// TODO Auto-generated method stub
 		return solitary_rate;
 	}
-
 }
