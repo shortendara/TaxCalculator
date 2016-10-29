29ae61b09ed194f9d3c311fe7dc6de8006321f3e
Dara Shorten <Dara Shorten>
2016-10-29 12:55:12 +0100

    included solitary tax rate in tax bands table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,18 @@
 CREATE TABLE  tax_bands(
-	
+	tax_rate_id int(11) NOT NULL,
+	excempt_tax_limit float,
+	lower_tax_rate float,
+	lower_tax_limit float,
+	middle_tax_rate float,
+	middle_tax_limit float,
+	higher_tax_rate float,
+	solidary_tax_rate float,
+	PRIMARY KEY(tax_rate_id)
 );
 
 CREATE TABLE citizens(
 	id int(11) NOT NULL AUTO_INCREMENT,
 	name varchar(100) NOT NULL,
-	salary float NOT_NULL,
+	salary float NOT NULL,
 	PRIMARY KEY(id)
 );
